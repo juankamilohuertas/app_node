cb1fa38845281bf323b06e7193b710ef9af6b3c3
Juan Camilo Huertas <juankamilohuertas2021@gmail.com>
2024-03-20 06:17:14 +0100

    la app esta lista para produccion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-const app = express() //ejecuta express
+const app = express(); //ejecuta express
 
-app.listen(3000) // puerto
+app.listen(process.env.PORT || 3000); // puerto
 
-app.get("/", (req,res)=>{
-    res.send("<h1>Hola Mundo</h1>")//envio de respuesta del cliente
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hola Mundo</h1>"); //envio de respuesta del cliente
+});
 /* otra pagina */
-app.get("/about", (req,res)=>{
-    res.send("<h1>acerca de</h1>")//envio de respuesta del cliente
-})
+app.get("/about", (req, res) => {
+  res.send("<h1>acerca de</h1>"); //envio de respuesta del cliente
+});
